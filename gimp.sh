19ecd104ed409789103c16887db5912cbb2a176e
Hendrik Schmitz <hendrik.schmitz03.sh@gmail.com>
2019-03-21 20:39:11 +0100

    Create gimp.sh

---
+++ gimp.sh
@@ -0,0 +1,11 @@
+# 1. load repository
+sudo add-apt-repository ppa:otto-kesselgulasch/gimp
+
+# 2. update repository
+sudo apt-get update
+
+# 3. Install gimp
+sudo apt-get install gimp
+
+# 4. Fix
+sudo apt-get install -f
